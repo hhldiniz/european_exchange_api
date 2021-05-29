3ba6505d79f42a049b7dcd46ef533d9bac2a6d3d
Hugo Diniz <hhldiniz@gmail.com>
2021-05-29 13:57:40 -0300

    Changing server no gunicorn in Procfile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from waitress import serve
 from flask import Flask
 
-from config import Config
 from currency.currency import currency_bp
 from history.history import history_bp
 from login.login import login_bp
@@ -12,7 +10,5 @@ if __name__ == "__main__":
     app.register_blueprint(currency_bp)
     app.register_blueprint(history_bp)
     app.register_blueprint(login_bp)
-    if Config.SERVER_ENV.value == "TEST":
-        app.run("localhost", 8080)
-    else:
-        serve(app, port=Config.PORT.value)
+    app.run()
+
