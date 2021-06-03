db26520e567f8dba4fe4c171864ed8cfea97babb
Hugo Diniz <hhldiniz@gmail.com>
2021-06-03 17:10:41 -0300

    Removing unwanted config variables

--- config.py
+++ config.py
@@ -7,8 +7,6 @@ load_dotenv()
 
 
 class Config(Enum):
-    LOCAL = os.getenv("LOCAL"),
-    REMOTE = os.getenv("REMOTE")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
