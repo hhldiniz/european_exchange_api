a570fdddf28a4da2a5018c658f6dae70a568b8ef
Hugo Diniz <hhldiniz@gmail.com>
2024-10-20 23:59:40 +0200

    Adapting code to new lambda function format in history.py

--- history/history.py
+++ history/history.py
@@ -1,5 +1,5 @@
 import json
-from typing import Optional, Any
+from typing import Optional, Any, List
 
 from logger import Logger
 from repository.currency_repository import CurrencyRepository
@@ -8,15 +8,22 @@ from terraform.project_dependencies.history.action import Action
 from util.json_encoder import MyJsonEncoder
 
 
-def get_history_lambda_handler(event: dict, context: Optional[dict]) -> dict[str, Any]:
+def get_history_lambda_handler(event: dict, _: Optional[dict]) -> dict[str, Any]:
     try:
+        Logger.i(f"Params: {event}")
         action: str = event["action"].upper()
         headers = {CommonHeaders.ContentType.value: "application/json"}
         ok_response_code = ResponseCodes.Status200.value[0]
+        start_at = event['start_at']
+        end_at = event['end_at']
+        base = event['base']
+        symbols = event['symbols'].split(',') if 'symbols' in event.keys() else None
         if action == Action.HISTORY.name:
-            return Response(status_code=ok_response_code, headers=headers, body=get_currency_history()).to_dict()
+            body = get_currency_history(base=base, start_at=start_at, end_at=end_at, symbols=symbols)
+            return Response(status_code=ok_response_code, headers=headers, body=body).to_dict()
         elif action == Action.LATEST.name:
-            return Response(status_code=ok_response_code, headers=headers, body=get_currency_latest()).to_dict()
+            body = get_currency_latest(symbol=symbols)
+            return Response(status_code=ok_response_code, headers=headers, body=body).to_dict()
         else:
             return Response(status_code=ResponseCodes.Status400.value[0], headers={}, body="BAD REQUEST").to_dict()
     except ValueError as e:
@@ -25,21 +32,14 @@ def get_history_lambda_handler(event: dict, context: Optional[dict]) -> dict[str
         return Response(status_code=ResponseCodes.Status500.value[0], headers={}, body=e.__str__()).to_dict()
 
 
-def get_currency_history() -> str:
+def get_currency_history(base: str, start_at: Optional[str], end_at: Optional[str], symbols: Optional[List[str]]) -> str:
     Logger.i("Request /history")
-    start_at = request.args.get('start_at')
-    end_at = request.args.get('end_at')
-    base = request.args.get('base')
-    symbols = request.args.get('symbols').split(',') if 'symbols' in request.args.keys() else None
     currency_repository = CurrencyRepository()
-
     return json.dumps(currency_repository.get_all(base, start_at, end_at, symbols), cls=MyJsonEncoder)
 
 
-def get_currency_latest() -> str:
+def get_currency_latest(symbol: str) -> str:
     Logger.i("Request /latest")
-    Logger.i(f"Params: {request.args}")
-    symbol = request.args.get("symbol")
     if symbol is None:
         raise ValueError("Missing argument")
     currency_repository = CurrencyRepository()
