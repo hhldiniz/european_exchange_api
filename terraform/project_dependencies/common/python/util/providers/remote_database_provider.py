271955410e1313686d3b6d5de307b1fa4dbc30d0
Hugo Diniz <hhldiniz@gmail.com>
2024-11-08 21:09:56 +0100

    Updating database url

--- terraform/project_dependencies/common/python/util/providers/remote_database_provider.py
+++ terraform/project_dependencies/common/python/util/providers/remote_database_provider.py
@@ -8,7 +8,6 @@ from util.providers.base_database_provider import BaseDatabaseProvider
 class RemoteDatabaseProvider(BaseDatabaseProvider):
     def get_connection(self) -> MongoClient:
         connection = pymongo.MongoClient(
-            f"mongodb+srv://{Config.DB_USER.value}:{Config.DB_PASSWORD.value}@exhange-api-cluster.yufz9.mongodb.net/"
-            "myFirstDatabase?retryWrites=true&w=majority")
+            f"mongodb+srv://{Config.DB_USER.value}:{Config.DB_PASSWORD.value}@exhange-api-cluster.9wao9.mongodb.net/"
+            f"?retryWrites=true&w=majority&appName=exhange-api-cluster")
         return connection
-
